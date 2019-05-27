ae98ba96fbd8c3bb0a301e0c1e2f6773fb19fa7b
libin <libincex@163.com>
2019-05-27 12:44:59 +0800

    add class oauth2

--- src/WeixinOauth2.php
+++ src/WeixinOauth2.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Libincex\WeixinApi;
 
 /**
@@ -6,9 +7,11 @@ namespace Libincex\WeixinApi;
  */
 class WeixinOauth2 extends WeixinBase
 {
-    protected $openid;
-    protected $userInfo;
+    protected $oauth2_access_token;
 
+    protected $openid;
+    protected $unionid;
+    protected $userInfo;
 
     //网页应用的授权
     //生成weixin用户登录页面的转向URL
@@ -28,52 +31,57 @@ class WeixinOauth2 extends WeixinBase
 
 
     //登录完成后，根据回调传进来的code,检测是否验证成功
-    public function getToken($code = NULL)
+    public function getAccessToken($code = NULL)
     {
-        $code = trim(isset($code) ? $code : $_REQUEST['code']);
-        if (empty($code)) {
-            return false;
+        if (empty($this->oauth2_access_token)) {
+
+            $code = trim(isset($code) ? $code : $_REQUEST['code']);
+            if (empty($code)) {
+                return false;
+            }
+
+            $url = 'https://api.weixin.qq.com/sns/oauth2/access_token?';
+            $data = array(
+                'appid' => $this->getAppId(),
+                'secret' => $this->getAppSecret(),
+                'code' => $code,
+                'grant_type' => 'authorization_code',
+            );
+
+            $url = $url . http_build_query($data);
+            $redata = $this->get($url);
+            $redata = json_decode(trim($redata), 1);
+            if (!is_array($redata) || empty($redata)) {
+                return false;
+            }
+
+            if (isset($redata['access_token'])) {
+                $this->oauth2_access_token = trim($redata['access_token']);
+                $this->openid = trim($redata['openid']);
+            }
+
         }
 
-        $url = 'https://api.weixin.qq.com/sns/oauth2/access_token?';
-        $data = array(
-            'appid' => $this->getAppId(),
-            'secret' => $this->getAppSecret(),
-            'code' => $code,
-            'grant_type' => 'authorization_code',
-        );
-
-        $url = $url . http_build_query($data);
-        $redata = WeixinAPI::get($url);
-        $redata = json_decode(trim($redata), 1);
-        if (!is_array($redata) || empty($redata)) {
-            return false;
-        }
-
-        if (isset($redata['access_token'])) {
-            $this->setAccessToken(trim($redata['access_token']));
-            $this->openid = trim($redata['openid']);
-        }
-
-        return $this->getAccessToken();
+        return $this->oauth2_access_token;
     }
 
-
     //获取用户信息
-    function userinfo()
+    function getUserinfo()
     {
-        if (!empty($this->userInfo)) {
+        if (empty($this->userInfo)) {
             //获取用户信息
             $url = 'https://api.weixin.qq.com/sns/userinfo?' . http_build_query(array(
-                    'access_token' => $this->getAccessToken(),
+                    'access_token' => $this->oauth2_access_token,
                     'openid' => $this->openid, //用户的唯一标识
                     'lang' => 'zh_CN',
                 ));
-            $redata = WeixinAPI::get($url);
+            $redata = $this->get($url);
             $redata = json_decode($redata, 1);
             if (!is_array($redata) || empty($redata['openid'])) {
                 return [];
             }
+
+            $this->userInfo = $redata;
         }
 
         return $this->userInfo;
